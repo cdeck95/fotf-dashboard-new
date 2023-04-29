75fad43a3131dc8d050c6a9faed7051bfa012dbe
Chris Deck <deckchris95@gmail.com>
2023-04-29 15:30:39 -0400

    sidebar asset overview

--- src/components/AssetOverviewSidebar.tsx
+++ src/components/AssetOverviewSidebar.tsx
@@ -19,14 +19,51 @@ function AssetOverview() {
   const teddyNFTs = tokens.Teddies.tokens;
   const aiTedNFTs = tokens.AITeds.tokens;
 
+  const oneOfOnes = tokens.OneofOnes.tokens;
+  const birthCerts = tokens.BirthCertificates.tokens;
+  const traitTokens = tokens.TraitSwapTokens.tokens;
+
+
   return (
     <Box className="info-card">
     <Box className="row-even">
       <div className="info-card__title">Asset Overview</div>
       <Typography 
-        className="learnMoreBtn">
+        className="learnMore">
         Est Value. 6 ETH
         </Typography>
+    </Box>
+    <Box className="row-around">
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{tedNFTs?.length}</Typography>
+        <Typography className="aseet-type">Teds</Typography> 
+      </Box>
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{teddyNFTs?.length}</Typography>
+        <Typography className="aseet-type">Teddies</Typography> 
+      </Box>
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{aiTedNFTs?.length}</Typography>
+        <Typography className="aseet-type">AI Teds</Typography> 
+      </Box>
+    </Box>
+    <Box className="row-center-margin">
+      <Typography className="honeyBalanceBlack">{honeyBalance}</Typography>
+      <Typography className="honeyBalance"> $HNY</Typography>
+    </Box>
+    <Box className="row-around">
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{oneOfOnes?.length}</Typography>
+        <Typography className="aseet-type">One of Ones</Typography> 
+      </Box>
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{birthCerts?.length}</Typography>
+        <Typography className="aseet-type">Birth Certificates</Typography> 
+      </Box>
+      <Box className='col-no-space'>
+        <Typography className="asset-numbers">{traitTokens?.length}</Typography>
+        <Typography className="aseet-type">Trait Tokens</Typography> 
+      </Box>
     </Box>
   </Box>
   );
