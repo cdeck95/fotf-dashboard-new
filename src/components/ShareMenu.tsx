8628f24573ec47a04dd9af953ddcd496cbf255f9
Chris Deck <deckchris95@gmail.com>
2023-06-27 16:18:08 -0400

    download image change

--- src/components/ShareMenu.tsx
+++ src/components/ShareMenu.tsx
@@ -127,6 +127,12 @@ export default function ShareMenu(props: ShareMenuProps) {
               >  <TwitterIcon size={32} round/>  
               </TwitterShareButton>
         </MenuItem>
+        <MenuItem onClick={() => downloadImage(token)}>
+          <Typography variant="body2" color="text.secondary"
+          sx={{ fontSize: ".93rem ", overflowWrap: 'break-word !important', whiteSpace: 'normal', wordWrap: 'break-word', maxWidth: "100%" }}>
+            Make sure to download the image & attach it to the post!
+          </Typography>
+        </MenuItem>
        
            
       </MenuList>
